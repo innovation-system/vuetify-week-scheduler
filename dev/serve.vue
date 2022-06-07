0ede5de14b0c3dcd8167ac714a841b74f677b616
reekoz <reekoz@gmail.com>
2022-06-07 12:07:56 +0200

    chore: initialize

---
+++ dev/serve.vue
@@ -0,0 +1,17 @@
+<script>
+import Vue from 'vue';
+import VuetifyWeekScheduler from '@/vuetify-week-scheduler.vue';
+
+export default Vue.extend({
+  name: 'ServeDev',
+  components: {
+    VuetifyWeekScheduler
+  }
+});
+</script>
+
+<template>
+  <div id="app">
+    <vuetify-week-scheduler />
+  </div>
+</template>
